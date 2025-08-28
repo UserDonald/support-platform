1d844f74b1a87331db16bfd3a77f720cbaaa0b60
djnash <djnash@iu.edu>
2025-08-27 23:50:45 -0400

    refactor: restructure authentication components and implement dashboard layout

---
+++ apps/web/app/(dashboard)/layout.tsx
@@ -0,0 +1,6 @@
+import { AuthGuard } from '@/modules/auth/ui/components/auth-guard';
+import { ReactNode } from 'react';
+
+export default function Layout({ children }: { children: ReactNode }) {
+  return <AuthGuard>{children}</AuthGuard>;
+}
