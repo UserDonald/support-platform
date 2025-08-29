48abddd92011b0901aac950e2ef7dcc571939868
djnash <djnash@iu.edu>
2025-08-29 05:28:44 -0400

    feat: refactor widget page to use async component and add new header and footer components

--- apps/widget/app/page.tsx
+++ apps/widget/app/page.tsx
@@ -1,28 +1,20 @@
-'use client';
+import { WidgetView } from '@/modules/widget/ui/views/widget-view';
+import { Suspense } from 'react';
 
-import { useVapi } from '@/modules/widget/hooks/use-vapi';
-import { Button } from '@workspace/ui/components/button';
+interface Props {
+  searchParams: Promise<{ organizationId: string }>;
+}
 
-export default function Page() {
-  const {
-    isSpeaking,
-    isConnecting,
-    isConnected,
-    transcript,
-    startCall,
-    endCall,
-  } = useVapi();
-
+export default async function Page(props: Props) {
   return (
-    <div className="flex flex-col items-center justify-center min-h-svh max-w-md mx-auto w-full">
-      <Button onClick={() => startCall()}>Start call</Button>
-      <Button onClick={() => endCall()} variant="destructive">
-        End call
-      </Button>
-      <p>isConnected: {`${isConnected}`}</p>
-      <p>isConnecting: {`${isConnecting}`}</p>
-      <p>isSpeaking: {`${isSpeaking}`}</p>
-      <p>{JSON.stringify(transcript, null, 2)}</p>
-    </div>
+    <Suspense>
+      <SuspendedComponent {...props} />
+    </Suspense>
   );
+}
+
+async function SuspendedComponent({ searchParams }: Props) {
+  const { organizationId } = await searchParams;
+
+  return <WidgetView organizationId={organizationId} />;
 }
